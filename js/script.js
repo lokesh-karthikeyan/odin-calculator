aee5fe77e162732065c2bb00fcfa6b4ddfabd7fe
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2024-09-01 19:57:24 +0530

    Add an event for `keydown` & `click`
     + Created an event for both "keydown" and "click" events.
     + The "getPressesTarget()" function will pass the target to "keyHandler()" function.
     + The "getClickedTarget()" function will pass the HTML element's "data-key" attribute's value.
    
    Both of these events are centralized towards another function.
    To avoid redundant lines, created a new central function which will handle keys for both events.

--- js/script.js
+++ js/script.js
@@ -14,3 +14,17 @@ function sayGreeting() {
     display.textContent = "0";
   }, 600);
 }
+
+document.addEventListener("keydown", getPressedTarget);
+
+function getPressedTarget(e) {
+  keyHandler(e.key);
+}
+
+let buttons = document.querySelector(".calc-buttons");
+
+buttons.addEventListener("click", getClickedTarget);
+
+function getClickedTarget(e) {
+  keyHandler(e.target.getAttribute("data-key"));
+}
